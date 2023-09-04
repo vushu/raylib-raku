9f7645e59d0c1792c3dc13be06e27db8465bb33f
davu <davu@mir-robots.com>
2023-09-05 01:33:57 +0200

    updated readme

--- generator/build.sh
+++ generator/build.sh
@@ -2,4 +2,4 @@
 
 #Please copy you libraylib.so into here
 gcc -c -fPIC raylib_pointerized_wrapper.c raylib_raku_helpers.c
-gcc -shared -o libraylib.so.4.5.0 raylib_raku_helpers.o raylib_pointerized_wrapper.o -lraylib
+gcc -shared -o libraylib.so.* raylib_raku_helpers.o raylib_pointerized_wrapper.o -lraylib
